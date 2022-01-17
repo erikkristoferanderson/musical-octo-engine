91c4e4d18450fdb460dd6a1feea1d3f62ac8980d
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2022-01-17 11:15:16 -0600

    first draft

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter
+from util import octo_sounds
+
+
+def main():
+    def start():
+        octo_sounds.go(var1, var2, var3, var4, var5, var6, var7, var8)
+    print("hello 1")
+    master = tkinter.Tk()
+    var1 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="1", variable=var1).grid(row=0, column=0, sticky=tkinter.W)
+    var2 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="2", variable=var2).grid(row=0, column=1, sticky=tkinter.W)
+    var3 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="3", variable=var3).grid(row=0, column=2, sticky=tkinter.W)
+    var4 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="4", variable=var4).grid(row=1, column=0, sticky=tkinter.W)
+    var5 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="5", variable=var5).grid(row=1, column=2, sticky=tkinter.W)
+    var6 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="6", variable=var6).grid(row=2, column=0, sticky=tkinter.W)
+    var7 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="7", variable=var7).grid(row=2, column=1, sticky=tkinter.W)
+    var8 = tkinter.IntVar()
+    tkinter.Checkbutton(master, text="8", variable=var8).grid(row=2, column=2, sticky=tkinter.W)
+    start_button = tkinter.Button(text="go", command=start).grid(row=1, column=1)
+    tkinter.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ util/octo_sounds.py
@@ -0,0 +1,56 @@
+import synthesizer
+from synthesizer import Player, Synthesizer, Waveform
+
+synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=0.3, use_osc2=False)
+player = Player()
+player.open_stream()
+
+
+def play_a_sound():
+    print('sound!')
+
+
+def play_a_note(note):
+    player.play_wave(synthesizer.generate_constant_wave(note, 1.0))
+
+
+def play_notes(notes_to_play):
+    player.play_wave(synthesizer.generate_chord(notes_to_play, 1.0))
+
+
+def go(var1, var2, var3, var4, var5, var6, var7, var8):
+    val_1 = var1.get()
+    val_2 = var2.get()
+    val_3 = var3.get()
+    val_4 = var4.get()
+    val_5 = var5.get()
+    val_6 = var6.get()
+    val_7 = var7.get()
+    val_8 = var8.get()
+
+    vals = [val_1, val_2, val_3, val_4, val_5, val_6, val_7, val_8]
+    if sum(vals) == 8 or sum(vals) == 4 or sum(vals) == 1:
+        notes = ["C3", "E3", "G3", "C4", "D4", "E4", "G4", "A4"]
+    elif sum(vals) == 7 or sum(vals) == 2:
+        notes = ["C3", "Eb3", "G3", "C4", "D4", "Eb4", "G4", "A4"]
+    elif sum(vals) == 6 or sum(vals) == 3:
+        notes = ["C3", "F3", "Ab3", "C4", "Eb4", "F4", "Ab4", "Bb4"]
+    elif sum(vals) == 5:
+        notes = ["C3", "Eb3", "G3", "C4", "D4", "Eb4", "G4", "A4"]
+    else:
+        notes = ["C3", "E3", "G3", "C4", "D4", "E4", "G4", "A4"]
+
+
+    zipped_notes_and_vals = zip(notes, vals)
+
+    notes_to_play = [note for note, val in zipped_notes_and_vals if val]
+    print(notes_to_play)
+    play_notes(notes_to_play)
+    
+    # for val, note in zip(vals, notes):
+    #     if val:
+    #         play_a_note(note)
+    #
+    # if val_1 and val_5:
+    #     play_a_sound()
+
